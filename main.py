d0a0f7a9bccd8a2ee29154470637f6a3760c7375
Sayreks <v.bezgubovv1@gmail.com>
2023-12-19 01:32:41 +0500

    start

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+# Создание экземпляра класса для работы с API сайтов с вакансиями
+hh_api = HeadHunterAPI()
+superjob_api = SuperJobAPI()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Получение вакансий с разных платформ
+hh_vacancies = hh_api.get_vacancies("Python")
+superjob_vacancies = superjob_api.get_vacancies("Python")
+
+# Создание экземпляра класса для работы с вакансиями
+vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
+
+# Сохранение информации о вакансиях в файл
+json_saver = JSONSaver()
+json_saver.add_vacancy(vacancy)
+json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
+json_saver.delete_vacancy(vacancy)
+
+# Функция для взаимодействия с пользователем
+def user_interaction():
+    platforms = ["HeadHunter", "SuperJob"]
+    search_query = input("Введите поисковый запрос: ")
+    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
+    filtered_vacancies = filter_vacancies(hh_vacancies, superjob_vacancies, filter_words)
+
+    if not filtered_vacancies:
+        print("Нет вакансий, соответствующих заданным критериям.")
+        return
+
+    sorted_vacancies = sort_vacancies(filtered_vacancies)
+    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
+    print_vacancies(top_vacancies)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    user_interaction()
